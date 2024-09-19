0b400d680a665618510182a7901aa22db2b20a7a
Professor-Masha <masha_e.maina@outlook.com>
2024-09-19 06:41:31 -0700

    0x06-regular_expressions

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,16 +1,2 @@
 #!/usr/bin/env ruby
-
-# Check if an argument is provided
-if ARGV.length != 1
-  puts "Usage: ./0-simply_match_school.rb 'your_string'"
-  exit
-end
-
-input_string = ARGV[0]
-
-# Match the word "School" using regex
-matches = input_string.scan(/School/)
-
-# Print the matched string or nothing if no match
-puts matches.join
-
+puts ARGV[0].scan(/hbt{2,5}n/).join
