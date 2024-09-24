1d22ed0db9aae3f44f1b6211c8f0508e913dc948
Professor-Masha <masha_e.maina@outlook.com>
2024-09-24 01:40:27 -0700

    0x0A-configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,12 @@
-# This Puppet manifest installs Flask version 2.1.0 using pip3
+# This Puppet manifest installs Flask version 2.1.0 and Werkzeug version 1.0.1 using pip3
 
 package { 'Flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '1.0.1',
   provider => 'pip3',
 }
 
