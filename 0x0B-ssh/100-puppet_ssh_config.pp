b644d76051ae575b6d21974c5210c3af101b8e4f
Professor-Masha <masha_e.maina@outlook.com>
2024-09-24 02:46:55 -0700

    SSH

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,35 +1,13 @@
-# Puppet manifest to configure SSH client settings
+# This Puppet manifest configures the SSH client to use the private key ~/.ssh/school
+# and disables password authentication.
 
-# Ensure the .ssh directory exists
-file { '/home/ubuntu/.ssh':
-  ensure => directory,
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-  mode   => '0700',
-}
-
-# SSH config file
-file { '/home/ubuntu/.ssh/config':
+file { '/etc/ssh/ssh_config':
   ensure  => file,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  mode    => '0600',
-  content => @("END_OF_CONTENT"),
-  replace => true,
-}
-
-# Add the necessary configuration to the SSH config file
-file_line { 'Turn off password auth':
-  path  => '/home/ubuntu/.ssh/config',
-  line  => 'PasswordAuthentication no',
-  match => 'PasswordAuthentication',
-  ensure => present,
-}
-
-file_line { 'Declare identity file':
-  path  => '/home/ubuntu/.ssh/config',
-  line  => 'IdentityFile ~/.ssh/school',
-  match => 'IdentityFile',
-  ensure => present,
+  content => @("EOF"),
+    # SSH client configuration
+    Host *
+      IdentityFile ~/.ssh/school
+      PasswordAuthentication no
+  EOF
 }
 
